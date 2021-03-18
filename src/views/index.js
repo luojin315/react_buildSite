e954d71da0428b62b3509611970c9c5836ed9506
wenrouxiansheng <773559646@qq.com>
2021-03-19 00:47:28 +0800

    "增加消息发布订阅 以及动画库"

--- src/views/index.js
+++ src/views/index.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react'
 import getComponent from '../exportComponent'
 import SuspendedWindow from '../editComponents/control_suspendedWindow/control_suspendedWindow'
+import PubSub from 'pubsub-js'
 
 export default class index extends Component {
     state = {
         ary: ["header", "nav"],
         isShowWindow: true
     }
-    test = () => {
-        this.setState({
-            ary: [...this.state.ary, 'nav']
-        })
-    }
-    openWindow = (data) => {
-        return () => {
-            this.setState({
-                isShowWindow: data
-            })
-        }
+    openWindow = () => {
+        //发布消息给订阅者
+        PubSub.publish('changeSuspendedWindowState', { isShowWindow: true });
     }
     render() {
-        const { ary, isShowWindow } = this.state;
+        const { ary } = this.state;
         return (
             <div>
                 {
@@ -28,8 +21,8 @@ export default class index extends Component {
                         return getComponent(item, index)
                     })
                 }
-                    <SuspendedWindow  key="1" openWindow={this.openWindow} chengeState={isShowWindow}/>
-                <button onClick={this.openWindow(true)}>click me</button>
+                <SuspendedWindow />
+                <button onClick={this.openWindow}>click me</button>
             </div >
         )
     }
